5b568be2529cf6f8aab6c157a86f1c9038b08269
Anil Singh <eraniljamwal@gmail.com>
2026-06-05 12:00:49 +0530

    test commit

--- bgp-react/src/App.tsx
+++ bgp-react/src/App.tsx
@@ -3,12 +3,7 @@ import { useEffect, useState } from "react";
 function App() {
   const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    fetch("/api/message")   // This calls the Worker API
-      .then(res => res.json())
-      .then(data => setMessage(data.message))
-      .catch(err => setMessage("Error: " + err.message));
-  }, []);
+  
 
   return (
     <div>
